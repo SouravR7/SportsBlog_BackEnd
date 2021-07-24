2c5ab5c1bb8850077236969523323fb8fd37f4fe
sourav.routh <saurav.rauth@nssplindia.com>
2021-07-24 11:27:26 +0530

    img upload modified according to frontend just experiment

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ const upload = multer({
 // const upload = multer({dest: 'uploads/'})
 
 app.post("/api/create",upload.single('imgURL'), (req, res) => {
-  const { description, title,date } = req.body;
-  console.log(req.file.path);
+  const { description, title,date,imgData } = req.body;
+  console.log(imgData);
   const newdata = new data_Collection({
-    imgURL : `https://sportsblog-backend.herokuapp.com/${req.file.path}`,
+    imgURL : `https://sportsblog-backend.herokuapp.com/${imgData}`,
     description,
     title,
     date,

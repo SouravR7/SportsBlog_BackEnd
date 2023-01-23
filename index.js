cf1120a36567a20ecd0a2f1971bb266240e68837
Sourav Routh <reactjs.shantinfosoft@gmail.com>
2023-01-23 20:06:11 +0530

    cors fixing

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const express = require("express");
-const { cloudinary } = require('./utils/cloudinary_config');
-const path = require('path');
+const { cloudinary } = require("./utils/cloudinary_config");
+const path = require("path");
 const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const bcrypt = require("bcryptjs");
 const connectMultiparty = require("connect-multiparty");
-const port = process.env.PORT ||7000;
+const port = process.env.PORT || 7000;
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 //app.use(express.static(__dirname))
 const { user_Collection, data_Collection } = require("./connector");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 //app.use(connectMultiparty());
-const multer = require('multer'); //for file read
+const multer = require("multer"); //for file read
 
-
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   next();
-})
-
+});
 
 //renaming & store the each upload image with date&time
 // const storage = multer.diskStorage({
@@ -60,13 +67,8 @@ app.use((req,res,next)=>{
 
 // const upload = multer({dest: 'uploads/'})
 
-
-
-
-
-app.use('/article', require('./routes/articleRoutes'))
-app.use('/user', require('./routes/userRoutes'))
-
+app.use("/article", require("./routes/articleRoutes"));
+app.use("/user", require("./routes/userRoutes"));
 
 //start the server locally
 app.listen(port, () => console.log(`App listen on port ${port}`));
